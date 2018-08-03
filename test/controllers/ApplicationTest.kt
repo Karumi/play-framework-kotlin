12fdc4ae2962c4987958a1ad38de085184db24a3
tonilopezmr <tonilopezmr@gmail.com>
2018-08-03 16:19:18 +0200

    Refactor ApplicationTest

--- test/controllers/ApplicationTest.kt
+++ test/controllers/ApplicationTest.kt
@@ -1,5 +1,6 @@
 package controllers
 
+import developers.NewDeveloperJson
 import developers.domain.Developer
 import developers.storage.DeveloperDao
 import given.GivenDeveloper
@@ -10,6 +11,7 @@ import play.mvc.Http.Status.BAD_REQUEST
 import play.mvc.Http.Status.CREATED
 import play.mvc.Http.Status.NOT_FOUND
 import play.mvc.Http.Status.OK
+import play.mvc.Result
 import play.test.Helpers.fakeRequest
 import play.test.Helpers.route
 import utils.ApplicationWithDatabase
@@ -24,8 +26,7 @@ class ApplicationTest : ApplicationWithDatabase(), ParseableJson, GivenDeveloper
   fun `developer POST should create a developer if it's a karumi developer`() {
     val newDeveloper = givenNewKarumiDeveloper()
 
-    val result = route(app, fakeRequest("POST", "/developer")
-      .bodyJson(newDeveloper.toJson()))
+    val result = postDeveloperRoute(newDeveloper)
 
     val createdDeveloper = result.asObject(Developer::class)
     val developer = Developer(createdDeveloper.id, newDeveloper.username, newDeveloper.email)
@@ -39,17 +40,14 @@ class ApplicationTest : ApplicationWithDatabase(), ParseableJson, GivenDeveloper
   fun `developer POST shouldn't create a developer if it isn't a karumi developer`() {
     val newDeveloper = givenNewDeveloper()
 
-    val result = route(app, fakeRequest("POST", "/developer")
-      .bodyJson(newDeveloper.toJson()))
+    val result = postDeveloperRoute(newDeveloper)
 
     assertEquals(BAD_REQUEST, result.status())
   }
 
-
   @Test
-  fun `developer POST should returns 400 if the json is bad formed`() {
-    val result = route(app, fakeRequest("POST", "/developer")
-      .bodyJson(InvalidJson().toJson()))
+  fun `developer POST should returns 400 if the json body isn't the expected`() {
+    val result = postDeveloperRoute(InvalidJson())
 
     assertEquals(BAD_REQUEST, result.status())
   }
@@ -58,7 +56,7 @@ class ApplicationTest : ApplicationWithDatabase(), ParseableJson, GivenDeveloper
   fun `developer GET should retrieve by id`() {
     val developer = givenDeveloper().let { create(it) }
 
-    val result = route(app, fakeRequest("GET", "/developer/${developer.id}"))
+    val result = getDeveloperRoute(developer)
 
     assertEquals(developer, result.asObject(Developer::class))
     assertEquals(OK, result.status())
@@ -68,12 +66,18 @@ class ApplicationTest : ApplicationWithDatabase(), ParseableJson, GivenDeveloper
   fun `developer GET should returns 404 code if there isn't the developer in the database`() {
     val developer = givenDeveloper()
 
-    val result = route(app, fakeRequest("GET", "/developer/${developer.id}"))
+    val result = getDeveloperRoute(developer)
 
     assertEquals(NOT_FOUND, result.status())
   }
 
-  data class InvalidJson(val invalid: String = "")
-  fun getById(id: UUID) = dao.getById(id)
-  fun create(developer: Developer) = dao.create(developer)
+  private fun getDeveloperRoute(developer: Developer): Result =
+    route(app, fakeRequest("GET", "/developer/${developer.id}"))
+
+  private fun <A> postDeveloperRoute(body: A): Result =
+    route(app, fakeRequest("POST", "/developer").bodyJson(body.toJson()))
+
+  private data class InvalidJson(val invalid: String = "")
+  private fun getById(id: UUID) = dao.getById(id)
+  private fun create(developer: Developer) = dao.create(developer)
 }
